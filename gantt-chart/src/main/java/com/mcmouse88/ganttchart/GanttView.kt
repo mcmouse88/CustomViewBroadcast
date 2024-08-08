c124d36d9590a327b8d51738ebce9aeb0ede6fef
razdobudko.s <razdobudko.s@wb.ru>
2024-08-08 02:35:25 +0200

    Draw tasks

--- gantt-chart/src/main/java/com/mcmouse88/ganttchart/GanttView.kt
+++ gantt-chart/src/main/java/com/mcmouse88/ganttchart/GanttView.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
@@ -32,11 +33,32 @@ class GanttView @JvmOverloads constructor(
         color = ContextCompat.getColor(context, R.color.grey_500)
     }
 
+    // For task shapes
+    private val taskShapePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        color = ContextCompat.getColor(context, R.color.blue_700)
+    }
+
+    // For task names
+    private val taskNamePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textSize = resources.getDimension(R.dimen.gantt_task_name_text_size)
+        color = Color.WHITE
+    }
+
     // Column width with the period
     private val periodWidth = resources.getDimensionPixelSize(R.dimen.gantt_period_width)
 
     // Row height
     private val rowHeight = resources.getDimensionPixelSize(R.dimen.gantt_row_height)
+
+    // Task corner radius
+    private val taskCornerRadius = resources.getDimension(R.dimen.gantt_task_corner_radius)
+
+    // Task vertical margin inside a row
+    private val taskVerticalMargin = resources.getDimension(R.dimen.gantt_task_vertical_margin)
+
+    // Task text horizontal margin inside its shape
+    private val taskTextHorizontalMargin = resources.getDimension(R.dimen.gantt_task_text_horizontal_margin)
 
     // Alternating row colors
     private val rowColors = listOf(
@@ -54,10 +76,13 @@ class GanttView @JvmOverloads constructor(
     private val periods = initPeriods()
 
     private var tasks: List<Task> = emptyList()
+    private var uiTasks: List<UiTask> = emptyList()
 
     fun setTasks(tasks: List<Task>) {
         if (tasks != this.tasks) {
             this.tasks = tasks
+            uiTasks = tasks.map(::UiTask)
+            updateTasksRects()
 
             // Notify to recalculated sizes
             requestLayout()
@@ -94,11 +119,18 @@ class GanttView @JvmOverloads constructor(
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         // Size has been changed, it's necessary to recalculate row width
         rowRect.set(0, 0, w, rowHeight)
+        // And tasks rect
+        updateTasksRects()
+    }
+
+    private fun updateTasksRects() {
+        uiTasks.forEachIndexed { index, uiTask -> uiTask.updateRect(index) }
     }
 
     override fun onDraw(canvas: Canvas) = with(canvas) {
         drawRows()
         drawPeriods()
+        drawTasks()
     }
 
     private fun Canvas.drawRows() {
@@ -136,6 +168,32 @@ class GanttView @JvmOverloads constructor(
         }
     }
 
+    private fun Canvas.drawTasks() {
+        uiTasks.forEach { uiTask ->
+            if (uiTask.isRectOnScreen) {
+                val taskRect = uiTask.rect
+                val taskName = uiTask.task.name
+
+                // Drawing shape
+                drawRoundRect(taskRect, taskCornerRadius, taskCornerRadius, taskShapePaint)
+
+                // Name position
+                val textX = taskRect.left + taskTextHorizontalMargin
+                val textY = taskNamePaint.getTextBaselineByCenter(taskRect.centerY())
+
+                // Symbols count from text, which will fit in the shape
+                val charCount = taskNamePaint.breakText(
+                    taskName,
+                    true,
+                    taskRect.width() - taskTextHorizontalMargin * 2,
+                    null
+                )
+//                drawText(taskName.substring(startIndex = 0, endIndex = charCount), textX, textY, taskNamePaint)
+                drawText(taskName, textX, textY, taskNamePaint)
+            }
+        }
+    }
+
     private fun Paint.getTextBaselineByCenter(center: Float): Float {
         return center - (descent() + ascent()) / 2
     }
@@ -155,6 +213,31 @@ class GanttView @JvmOverloads constructor(
         }
     }
 
+    private inner class UiTask(val task: Task) {
+        val rect = RectF()
+
+        val isRectOnScreen: Boolean
+            get() = rect.top < height && (rect.right > 0 || rect.left < rect.width())
+
+        fun updateRect(index: Int) {
+            fun getX(date: LocalDate): Float? {
+                val periodIndex = periods.getValue(periodType).indexOf(periodType.getDateString(date))
+                return if (periodIndex >= 0) {
+                    periodWidth * (periodIndex + periodType.getPercentOfPeriod(date))
+                } else {
+                    null
+                }
+            }
+
+            rect.set(
+                getX(task.dateStart) ?: -taskCornerRadius,
+                rowHeight * (index + 1f) + taskVerticalMargin,
+                getX(task.dateEnd) ?: (width + taskCornerRadius),
+                rowHeight * (index + 2f) - taskVerticalMargin
+            )
+        }
+    }
+
     companion object {
         private const val MONTH_COUNT = 2L
     }
@@ -163,6 +246,9 @@ class GanttView @JvmOverloads constructor(
         MONTH {
             override fun increment(date: LocalDate): LocalDate = date.plusMonths(1)
             override fun getDateString(date: LocalDate): String = date.month.name
+            override fun getPercentOfPeriod(date: LocalDate): Float {
+                return (date.dayOfMonth - 1f) / date.lengthOfMonth()
+            }
         },
 
         WEEK {
@@ -171,9 +257,14 @@ class GanttView @JvmOverloads constructor(
             override fun getDateString(date: LocalDate): String {
                 return date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR).toString()
             }
+
+            override fun getPercentOfPeriod(date: LocalDate): Float {
+                return (date.dayOfWeek.value - 1f) / 7
+            }
         };
 
         abstract fun increment(date: LocalDate): LocalDate
         abstract fun getDateString(date: LocalDate): String
+        abstract fun getPercentOfPeriod(date: LocalDate): Float
     }
 }
